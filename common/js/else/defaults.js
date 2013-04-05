72e477fb63e416ca41a18e42d6a7f5ad1b3152a5
NeverI <neverii@freemail.hu>
2013-04-05 19:08:57 +0200

    Defaults: spelling error fix (refs #11)

--- common/js/else/defaults.js
+++ common/js/else/defaults.js
@@ -11,33 +11,33 @@ var defaultOptions = {
       todosIsActive: 'true',
       isVerboseSiteWatcher: 'true',
       viceDomains: 'reddit.com\n9gag.com\nfacebook.com',
-      goodDomains: 'lifehacker.com\ncodeacadamy.com\nkhanacadamy.org',
+      goodDomains: 'lifehacker.com\ncodeacademy.com\nkhanacademy.org',
       days: {
-          'Monday': { 
+          'Monday': {
               active: true,
               start: [8,0], end: [16,30]
             },
-          'Tuesday': { 
+          'Tuesday': {
               active: true,
               start: [8,0], end: [16,30]
             },
-          'Wednesday': { 
+          'Wednesday': {
               active: true,
               start: [8,0], end: [16,30]
             },
-          'Thursday': { 
+          'Thursday': {
               active: true,
               start: [8,0], end: [16,30]
             },
-          'Friday': { 
+          'Friday': {
               active: true,
               start: [8,0], end: [16,30]
             },
-          'Saturday': { 
+          'Saturday': {
               active: false,
               start: [8,0], end: [16,30]
             },
-          'Sunday': { 
+          'Sunday': {
               active: false,
               start: [8,0], end: [16,30]
             }
